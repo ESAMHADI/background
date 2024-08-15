7693e945bd4fbddf705c5df44bab5688f673ef19
ESAM <alshawshsam@gmail.com>
2024-08-15 18:42:49 +0300

    adding starting

---
+++ script1.js
@@ -0,0 +1,122 @@
+function sayHello() {
+	console.log("Hello");
+}
+sayHello();
+
+var sayBye = function byebye() {
+	console.log("bye");}
+	sayBye(); 
+
+function sign() {
+console.log("hiiiiiiiiiiiii");
+console.log("helooooooooooo");
+}
+sign()
+
+function sign2() {
+console.log("hmmmmmmmmmmmmm");
+console.log("gooooooooooooo");
+}
+sign2()
+
+function sign2(song) {
+console.log(song);}
+sign2("i am heeeeeere");
+sign2("where are you ");
+sign2(" who are you ");
+
+function multiply(a,b) {
+return a ;
+return a * b;
+return b;
+}
+multiply(5,60);
+
+function multiply(a,b) {
+return a * b;
+}
+multiply(5,60);
+
+function multiply(a,b) {
+if (a > 10 || b > 10) {
+return "that's so hard";
+} else { 
+return a*b;
+   }
+}
+multiply(5,60);
+
+function multiply(a,b) {
+return a*b;
+}
+alert(multiply(2,8));
+
+var total = multiply(4,7);
+alert(total);
+
+
+
+
+function checkDriverAge() {
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+alert("Powering On. Enjoy the ride!"); 
+}
+else if(Number(age)===18) {
+
+alert("Congratulations on your first year of driving. Enjoy the ride!");
+}}
+checkDriverAge()
+
+
+function checkDriverAge2() {
+var age = prompt("What is your age?");
+
+if (Number(age) < 18) {
+alert("Sorry, you are too yound to drive this car. Powering off");
+} else if (Number(age) > 18) {
+alert("Powering On. Enjoy the ride!"); 
+}
+else if(Number(age)===18) {
+
+alert("Congratulations on your first year of driving. Enjoy the ride!");
+}}
+checkDriverAge2()
+
+
+
+
+
+/*Array*/
+
+/*var list = ["tiger","cat","bear","bird"];
+
+console.log(list[3]);
+*/
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
